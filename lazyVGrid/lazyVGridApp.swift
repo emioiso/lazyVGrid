4455df2038a9059206db5a0fa284a1eda2a0dc55
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-08 16:42:52 +0900

    Initial Commit

---
+++ lazyVGrid/lazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  lazyVGridApp.swift
+//  lazyVGrid
+//
+//  Created by emi oiso on 2023/10/08.
+//
+
+import SwiftUI
+
+@main
+struct lazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
